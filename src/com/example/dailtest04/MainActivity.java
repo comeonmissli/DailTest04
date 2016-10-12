7bd1fbd820c1a9a3e5dafbc8fbb25f277351fa24
comeonmissli <1312467852@qq.com>
2016-10-12 17:49:19 +0800

    Print"OK"

---
+++ src/com/example/dailtest04/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.dailtest04;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+   EditText editText;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		editText=(EditText) this.findViewById(R.id.editText);
+		Button myBtn=(Button) this.findViewById(R.id.myBtn);
+		myBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Uri uri=Uri.parse("tel:"+editText.getText().toString());
+				Intent intent=new Intent(Intent.ACTION_CALL,uri);
+				MainActivity.this.startActivity(intent);
+				
+			}
+		});
+	}
+   
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
